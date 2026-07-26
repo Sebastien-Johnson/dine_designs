52fc231ba350f56628ac3d175f0853febbd068aa
Sebastien Johnson <sebastien.z.johnson@gmail.com>
2026-07-26 16:15:42 -0400

    added auth statement to post creation

--- dj_blog/dine_designs/urls.py
+++ dj_blog/dine_designs/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("log_auth", TemplateView.as_view(template_name="log_auth.html"), name="log_auth"),
     path("", include("posts.urls")),
-    path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
 
 if settings.DEBUG:  # new
--- dj_blog/posts/models.py
+++ dj_blog/posts/models.py
@@ -12,3 +12,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title + " | " + str(self.author)
+
+    def get_author(self, request):
+        self.author = request.user
