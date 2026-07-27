17266fe0a8d455ee5fd15b4a84a0e4589f6555b7
Sebastien Johnson <sebastien.z.johnson@gmail.com>
2026-07-26 20:09:17 -0400

    Updating page routing and post auth

--- dj_blog/dine_designs/settings.py
+++ dj_blog/dine_designs/settings.py
@@ -121,8 +121,8 @@ STATIC_URL = 'static/'
 #Auth
 AUTH_USER_MODEL = "accounts.CustomUser"
 
-LOGIN_REDIRECT_URL = "home"
-LOGOUT_REDIRECT_URL = "home"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 #Media
