595f630ab56f1282685a910ce970258b6bcc5717
Sebastien <sebastien.z.johnson@gmail.com>
2026-07-27 16:36:00 -0400

    Added edit functionality to posts

--- dj_blog/posts/urls.py
+++ dj_blog/posts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView
+from . import views
+from .views import *
 
 urlpatterns = [
     path("<int:pk>/", PostDetailView.as_view(), name="post_detail"),
     path("post_create/", PostCreateView.as_view(), name="post_create"),
+    path('post_edit/<int:id>/', views.edit_post, name='post_edit'),
     path("", PostListView.as_view(), name="post_list"),
 ]
--- dj_blog/posts/views.py
+++ dj_blog/posts/views.py
@@ -1,7 +1,8 @@
 from django.views.generic import ListView, DetailView, CreateView
+from django.contrib import messages
 from django.urls import reverse_lazy
 from django.http import HttpResponseRedirect, request
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 from .forms import CreatePost
 
@@ -15,6 +16,7 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
+
 
 class PostCreateView(CreateView):
     model = Post
@@ -37,4 +39,21 @@ class PostCreateView(CreateView):
             return HttpResponseRedirect('post_list')
         else:
             form = CreatePost()
-        return render(request, 'upload.html', {'form': form})
+        return render(request, 'post_create.html', {'form': form})
+
+def edit_post(request, id):
+    post = get_object_or_404(Post, id=id)
+
+    if request.method == 'GET':
+        context = {'form': CreatePost(instance=post), 'id': id}
+        return render(request,'post_edit.html',context)
+
+    elif request.method == 'POST':
+        form = CreatePost(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'The post has been updated successfully.')
+            return redirect('post_list')
+        else:
+            messages.error(request, 'Please correct the following errors:')
+            return render(request,'post_edit.html',{'form':form})
