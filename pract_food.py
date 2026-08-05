3c506c0a42d8e2e52ba50c05b892bed38d3874f3
Sebastien <sebastien.z.johnson@gmail.com>
2026-08-05 15:00:09 -0400

    Updated to usda api

--- pract_food.py
+++ pract_food.py
@@ -1,10 +1,18 @@
-import noms
 import yaml
+import requests
+import json
+from requests.auth import HTTPBasicAuth
 
-with open("config.yaml", "r") as ymlfile:
-    cfg = yaml.safe_load(ymlfile)
+def get_food():
+    with open("config.yaml", "r") as ymlfile:
+        cfg = yaml.safe_load(ymlfile)
+        food = "Cheese"
+        key = str(cfg["usda_api_key"])
+       
+        headers={"x-api-key":key, "query":food}
+        url = f"https://api.nal.usda.gov/fdc/v1/foods/search?"
 
-client = noms.Client(cfg["noms_api_key"])
-
-search_results = client.search_query("Raw Broccoli")
-print(search_results)
+        response = requests.get(url, headers=headers)
+        print(json.dumps(response.json(), indent=2))
+        
+get_food()
