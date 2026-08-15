85511e168c5208040be07db337d0b512dac140a3
Sebastien <sebastien.z.johnson@gmail.com>
2026-08-14 22:09:58 -0400

    Began outlining test suite

--- dj_blog/posts/tests.py
+++ dj_blog/posts/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from models import Post, Rating, Comment
+from views import PostRatingView, AddCommentView, PostDeleteView, PostEditView, PostCreateView, PostDetailView, PostListView
 
-# Create your tests here.
+class PostTestCase(TestCase):
+    def setUp(self):
+        return super().setUp()
+
+class RatingTestCase(TestCase):
+    def setUp(self):
+        return super().setUp()
+
+class CommentTestCase(TestCase):
+    def setUp(self):
+        return super().setUp()
+
+class PostRatingTestCase(TestCase):
+    def setUp(self):
+        return super().setUp()
+
+class AddCommentTestCase(TestCase):
+    def setUp(self):
+        return super().setUp()
+
+class PostDeleteTestCase(TestCase):
+    def setUp(self):
+        return super().setUp()
+
+class PostEditTestCase(TestCase):
+    def setUp(self):
+        return super().setUp()
+
+class PostCreateTestCase(TestCase):
+    def setUp(self):
+        return super().setUp()
+
+class PostDetailTestCase(TestCase):
+    def setUp(self):
+        return super().setUp()
+
+class PostListTestCase(TestCase):
+    def setUp(self):
+        return super().setUp()
+    
